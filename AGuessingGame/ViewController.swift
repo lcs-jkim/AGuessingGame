1ab9a60f28b3917a2bad058b67e43b552d8ab174
Amanda <jkim@lcs.on.ca>
2019-09-30 11:32:53 -0400

    made some notes

--- AGuessingGame/ViewController.swift
+++ AGuessingGame/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
-
+    // MARK: Properties
+    
+    
+    // MARK: Initianlizers
+    
+    
+    // MARK: Methods (functions)
+    
+    // Runs as soon as the view becomes visible to the user
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Make an object named 'synthesizer', which is an instance of the class 'AVspeechSynthesizer' 
     }
 
 
